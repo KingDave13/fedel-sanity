f145c546756dc50653c0f2e0f890395d1f50ee8a
David Okaliwe <okaliwedavid@gmail.com>
2024-05-20 09:21:46 +0100

    Updates

--- fedel-tiles/schemaTypes/Attribute.js
+++ fedel-tiles/schemaTypes/Attribute.js
@@ -4,13 +4,49 @@ export default {
     type: 'document',
     fields: [
         { 
-            name: 'name', 
-            title: 'Name', 
+            name: 'price', 
+            title: 'Price', 
+            type: 'number' 
+        },
+        { 
+            name: 'variations', 
+            title: 'Variations', 
+            type: 'array', 
+            of: [{ type: 'string' }] 
+        },
+        { 
+            name: 'dimensions', 
+            title: 'Dimensions', type: 'object', 
+                fields: [
+                    { 
+                        name: 'width', 
+                        title: 'Width', 
+                        type: 'number' 
+                    },
+                    { 
+                        name: 'height', 
+                        title: 'Height', 
+                        type: 'number' 
+                    },
+        ]},
+        { 
+            name: 'manufacturer', 
+            title: 'Manufacturer', 
             type: 'string' 
         },
         { 
-            name: 'value', 
-            title: 'Value', 
+            name: 'type', 
+            title: 'Type', 
+            type: 'string' 
+        },
+        { 
+            name: 'application', 
+            title: 'Application', 
+            type: 'string' 
+        },
+        { 
+            name: 'material', 
+            title: 'Material', 
             type: 'string' 
         },
     ],
