9bd103ad3e8b2dcfb5c3093a7d81a16d95344670
David Okaliwe <okaliwedavid@gmail.com>
2024-05-20 09:12:03 +0100

    Updates

---
+++ fedel-tiles/schemaTypes/Attribute.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'attribute',
+    title: 'Attribute',
+    type: 'document',
+    fields: [
+        { 
+            name: 'name', 
+            title: 'Name', 
+            type: 'string' 
+        },
+        { 
+            name: 'value', 
+            title: 'Value', 
+            type: 'string' 
+        },
+    ],
+};
