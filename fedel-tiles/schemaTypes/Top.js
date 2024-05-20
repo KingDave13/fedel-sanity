8c2201b2736f7d7f5dc56da0e3c25c1b34589d88
David Okaliwe <okaliwedavid@gmail.com>
2024-05-20 22:09:00 +0100

    Updates

---
+++ fedel-tiles/schemaTypes/Top.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'top',
+    title: 'Top Materials',
+    type: 'document',
+    fields: [
+        { 
+            name: 'name', 
+            title: 'Name', 
+            type: 'string' 
+        },
+        { 
+            name: 'images', 
+            title: 'Images',
+            type: 'image'
+        },
+    ],
+};
--- fedel-tiles/schemaTypes/index.js
+++ fedel-tiles/schemaTypes/index.js
@@ -1,8 +1,9 @@
 export const schemaTypes = [
-    Product, Category, Subcategory, Attribute
+    Product, Category, Subcategory, Attribute, Top
 ];
 
 import Product from "./Product";
 import Category from "./Category";
 import Subcategory from "./Subcategory";
 import Attribute from "./Attribute";
+import Top from './Top';
