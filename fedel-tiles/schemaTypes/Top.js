a2f222c5161cc701e34595fb9cebf056578403fd
David Okaliwe <okaliwedavid@gmail.com>
2024-05-28 04:59:00 +0100

    Updates

--- fedel-tiles/schemaTypes/Top.js
+++ fedel-tiles/schemaTypes/Top.js
@@ -4,6 +4,11 @@ export default {
     type: 'document',
     fields: [
         {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+        },
+        {
             name: 'products',
             title: 'Products',
             type: 'array',
